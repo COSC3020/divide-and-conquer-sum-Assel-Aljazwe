d64023abc96de407833bc9cd5123f1b2177be44b
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-02-05 16:45:15 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function divideAndConquerSum(a) {
-    return a;
+    if (a.length <= 1) {
+        return a.length === 1 ? a[0] : 0;
+    }
+    //Dividing the array into three roughly equal parts
+    const oneThird = Math.ceil(a.length / 3);
+    const twoThirds = Math.ceil(2 * a.length / 3);
+
+    const first = a.slice(0, oneThird);|
+    const second = a.slice(oneThird, twoThirds);
+    const third = a.slice(twoThirds);
+
+    //Conquering the array recursively
+    const sumFirst = divideAndConquerSum(first);
+    const sumSecond = divideAndConquerSum(second);
+    const sumThird = divideAndConquerSum(third);
+
+    //Combining results of the recursive calls
+    return sumFirst + sumSecond + sumThird;
 }
