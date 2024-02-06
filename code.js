abf65a4b616c2983b60191a7d1b7457be8c23294
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-02-05 17:02:42 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,20 +1,21 @@
 function divideAndConquerSum(a) {
-    if (a.length <= 1) {
-        return a.length === 1 ? a[0] : 0;
-    }
-    //Dividing the array into three roughly equal parts
-    const oneThird = Math.ceil(a.length / 3);
-    const twoThirds = Math.ceil(2 * a.length / 3);
-
-    const first = a.slice(0, oneThird);|
-    const second = a.slice(oneThird, twoThirds);
-    const third = a.slice(twoThirds);
-
-    //Conquering the array recursively
-    const sumFirst = divideAndConquerSum(first);
-    const sumSecond = divideAndConquerSum(second);
-    const sumThird = divideAndConquerSum(third);
-
-    //Combining results of the recursive calls
-    return sumFirst + sumSecond + sumThird;
+    function sumHelper(arr, start, end){
+        if (start > end) 
+             return 0;
+        if (start === end)
+            return arr[start];
+        //Dividing the array into three roughly equal parts
+        const segmentLength = end - start + 1;
+        const oneThird = start + Math.floor(segmentLength / 3);
+        const twoThirds = start + 2 * Math.floor(segmentLength / 3);
+    
+        //Conquering the array recursively
+        const sumFirst = sumHelper(arr, start, oneThird - 1);
+        const sumSecond = sumHelper(arr, oneThird, twoThirds - 1);
+        const sumThird = sumHelper(arr, twoThirds, end);
+    
+        //Combining results of the recursive calls
+        return sumFirst + sumSecond + sumThird;
+        }
+        return sumHelper(a, 0, a.length - 1)
 }
